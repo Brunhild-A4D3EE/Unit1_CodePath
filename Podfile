b2de4a3c9b6e38fee937e9cc4989b2b35af56125
Suye Shen <suyeshen@gmail.com>
2021-09-25 04:49:28 -0400

    Finished Unit1 Assignment

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Unit1_CodePath' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Unit1_CodePath
+  pod "AlamofireImage"
+
+end
--- Unit1_CodePath/ViewController.swift
+++ Unit1_CodePath/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -61,6 +62,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        cell.posterView.af.setImage(withURL: posterUrl!)
         
         return cell
     }
